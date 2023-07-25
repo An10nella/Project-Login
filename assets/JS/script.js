8579df63e4302b60c5f3148a55d4ff9a40f082e7
An10nella <135998311+An10nella@users.noreply.github.com>
2023-07-25 13:08:03 +0300

    solving loop problem

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,55 +1,62 @@
 var users= [
     {
-    username01 :"admin01", 
+    username :"admin01", 
     password :"123"
 
 },
 {
-    username02 :"admin02" ,
+    username :"admin02" ,
     password: "234" 
 
 },
 {
-    username03 :"admin03" ,
+    username :"admin03" ,
     password :"345" 
     
 }
 ] 
 
-
+var maxAttempts = 2;
+var attempts = 0;
 // var btnPrimary = document.getElementById("btn btn-primary")
-var username = document.getElementById("name").value
-var password = document.getElementById("Password").value
-
 function Welcome(){
-  // for (i=0 ; i<users.length ; i++){
-    // if( counter == 0){
-    //     alert("You have reached "+attempt+" attempt;")
-    //  return 
-    // }
-
-     
+var username = document.getElementById("username").value
+var password = document.getElementById("password").value
+for (i=0; i<users.length; i++){
+  if(username==users[i].username && password == users[i].password){
+    alert("login successfully")
+    main()
     
 
-    if(username== users){
-
-   alert ("Login successfully")}}
+  }
   
-  //  window.location = "login.html"
-  //  displayWelcome(username)
-  //   setTimeout(() => {
-  //     window.location.href = "login.html"
-  //   }, 2000);
-  //  return 
-// }
-// else if(username!= users[i].username && password !=users[i].password){
+  
+// else i(username!= users[i].username && password !=users[i].password){
+    
+else{  
+    
+    var remainingAttempts = maxAttempts - attempts;
+    if (remainingAttempts >= 0 ) {
+      attempts++
+      
+      alert("Incorrect username or password. You have " + remainingAttempts + " attempt(s) remaining.")
+      return
+      
 
-//    alert("You have left "+attempt+" attempt;")
-//    Welcome(counter - 1)
-// }
-// }
+    }}
+  
 
-// }
+    
+}
+
+
+  }
+
+
+
+  
+
+
 // const loginPopup = document.querySelector(".main")
 // window.addEventListener("load",function(){
 // showPopup()
@@ -68,59 +75,62 @@ function Welcome(){
 
 // }
 
-// const fighters = [
-//     "🐉",
-//     "🐥",
-//     "🐊",
-//     "💩",
-//     "🦍",
-//     "🐢",
-//     "🐩",
-//     "🦀",
-//     "🐝",
-//     "🤖",
-//     "🐘",
-//     "🐸",
-//     "🕷",
-//     "🐆",
-//     "🦕",
-//     "🦁",
-//   ]
+function main(){
+const fighters = [
+    "🐉",
+    "🐥",
+    "🐊",
+    "💩",
+    "🦍",
+    "🐢",
+    "🐩",
+    "🦀",
+    "🐝",
+    "🤖",
+    "🐘",
+    "🐸",
+    "🕷",
+    "🐆",
+    "🦕",
+    "🦁",
+  ]
 
   
-//   let stageEl = document.getElementById("stage")
-//   let fightButtonEl = document.getElementById("fightButton")
+  let stageEl = document.getElementById("stage")
+  let fightButtonEl = document.getElementById("fightButton")
 
-//   fightButtonEl.addEventListener("click",function(){
+  fightButtonEl.addEventListener("click",function(){
 
-//     let char01 =  Math.floor(Math.random(fighters)*fighters.length)
-//     let char02 = Math.floor(Math.random(fighters)*fighters.length)
+    let char01 =  Math.floor(Math.random(fighters)*fighters.length)
+    let char02 = Math.floor(Math.random(fighters)*fighters.length)
 
-//     let diff = 0
+    let diff = 0
  
-//     stageEl.innerHTML = `${fighters[char01]} VS ${fighters[char02]}`
-//     if(fighters[3] == fighters[char01] || fighters[3] == fighters[char02]){
-//         stageEl.innerHTML += `<br> ${fighters[3]} is the winner`
-//       }else{
-//         if(char01 > char02){
-//           diff = char01 - char02
-//           stageEl.innerHTML += `<br> ${fighters[char01]} win by ${diff} points`
-//           if(diff>9){
-//             fightButtonEl.style.backgroundColor="#ff5"
-//           }else{
-//             fightButtonEl.style.backgroundColor="#fff"
-//           }
-//         }else if(char01 < char02){
-//           diff = char02 - char01
-//           stageEl.innerHTML += `<br> ${fighters[char02]} win by ${diff} points`
-//           if(diff>9){
-//             fightButtonEl.style.backgroundColor="#ff5"
-//           }else{
-//             fightButtonEl.style.backgroundColor="#fff"
-//           }
-//         }else{
-//           stageEl.innerHTML += `<br> Tie Game`
-//           fightButtonEl.style.backgroundColor="#f0f"
-//         }
-//       }
-//   })
+    stageEl.innerHTML = `${fighters[char01]} VS ${fighters[char02]}`
+       
+    if(fighters[3] == fighters[char01] || fighters[3] == fighters[char02]){
+        stageEl.innerHTML += `<br> ${fighters[3]} is the winner`
+      }else{
+        if(char01 > char02){
+          diff = char01 - char02
+          stageEl.innerHTML += `<br> ${fighters[char01]} win by ${diff} points`
+          if(diff>9){
+            fightButtonEl.style.backgroundColor="#ff5"
+          }else{
+            fightButtonEl.style.backgroundColor="#fff"
+          }
+        }else if(char01 < char02){
+          diff = char02 - char01
+          stageEl.innerHTML += `<br> ${fighters[char02]} win by ${diff} points`
+          if(diff>9){
+            fightButtonEl.style.backgroundColor="#ff5"
+          }else{
+            fightButtonEl.style.backgroundColor="#fff"
+          }
+        }else{
+          stageEl.innerHTML += `<br> Tie Game`
+          fightButtonEl.style.backgroundColor="#f0f"
+        }
+      }
+  })
+}
